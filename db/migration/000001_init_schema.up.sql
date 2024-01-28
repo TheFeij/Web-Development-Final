c6106affdeef39bed3d19f6b98bee9b1fb193ea3
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-28 18:52:29 +0330

    Added table for contacts

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -7,4 +7,15 @@ create table users (
     password varchar,
     image varchar unique,
     bio varchar(100)
-)
+);
+
+create table contacts (
+    user_id bigint,
+    contact_id bigint,
+    contact_name varchar(100),
+    display_number boolean default true,
+    display_profile_picture boolean default true,
+    primary key (user_id, contact_id),
+    foreign key (user_id) references users(id),
+    foreign key (contact_id) references users(id)
+);
