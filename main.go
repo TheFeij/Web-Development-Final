5761ab9bd5bac6c9436209318927add5ea7f967d
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 18:45:44 +0330

    Added code to connect to db and start the server on localhost:8080

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"Messenger/api"
+	"Messenger/db"
+	"database/sql"
+	"log"
+)
+
 func main() {
-	// TODO
+	db.Init()
+	db := db.GetDB()
+	DB, err := db.DB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func(DB *sql.DB) {
+		err := DB.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(DB)
+
+	server := api.NewServer(db)
+	err = server.Start("localhost:8080")
+	if err != nil {
+		log.Fatalln("Cannot start the server ", err.Error())
+		return
+	}
 }
