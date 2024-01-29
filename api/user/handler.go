bca123fa9939c290d12b0367f4743a975ac0c6ca
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 22:08:51 +0330

    completed registerUser route handler

--- api/user/handler.go
+++ api/user/handler.go
@@ -1,11 +1,16 @@
 package user
 
 import (
+	"Messenger/db/services"
 	"Messenger/requests"
+	"Messenger/responses"
+	"Messenger/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/gorm"
 	"net/http"
 	"path/filepath"
+	"time"
 )
 
 type Handler struct {
@@ -43,12 +48,46 @@ func (h Handler) RegisterUser(context *gin.Context) {
 	req.Image = imagePath
 
 	//save the user
+	service := services.New(h.db)
+	var res responses.RegisterUserResponse
+	res, err = service.RegisterUser(req)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
 
 	if err := context.SaveUploadedFile(file, imagePath); err != nil {
 		context.JSON(http.StatusInternalServerError, errResponse(err))
 		return
 	}
 
+	refreshToken, err := utils.NewToken(utils.UserClaims{
+		ID: res.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+		},
+	})
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	accessToken, err := utils.NewToken(utils.UserClaims{
+		ID: res.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+		},
+	})
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	context.Header("Authorization", accessToken)
+	context.Header("Refresh-Token", refreshToken)
+	context.JSON(http.StatusOK, res)
 }
 
 func errResponse(err error) gin.H {
