14efbad1959c6d8427fb3704d28f4c0d01c36df1
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 19:57:20 +0330

    Defined RegisterUser handler for api/register endpoint

--- api/user/handler.go
+++ api/user/handler.go
@@ -1,8 +1,11 @@
 package user
 
 import (
+	"Messenger/requests"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
+	"path/filepath"
 )
 
 type Handler struct {
@@ -13,6 +16,39 @@ func NewHandler(db *gorm.DB) *Handler {
 	return &Handler{
 		db: db,
 	}
+}
+
+func (h Handler) RegisterUser(context *gin.Context) {
+
+	var req requests.RegisterUser
+	if err := context.ShouldBind(&req); err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	// Handle image upload
+	file, err := context.FormFile("image")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	ext := filepath.Ext(file.Filename)
+	if ext != ".jpg" && ext != ".png" {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid file extension. Supported formats: jpg, png"})
+		return
+	}
+
+	imagePath := "./data/profile_images/" + req.Username + ext
+	req.Image = imagePath
+
+	//save the user
+
+	if err := context.SaveUploadedFile(file, imagePath); err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
 }
 
 func errResponse(err error) gin.H {
