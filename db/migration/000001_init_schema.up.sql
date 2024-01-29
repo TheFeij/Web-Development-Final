c271c5f02a3926e5c9df401ffaa3c65349df583a
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 19:03:26 +0330

    Set phone number to be exactly 11 chars

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,7 +2,7 @@ create table users (
     id bigint primary key,
     firstname varchar(50),
     lastname varchar(50),
-    phone varchar(11) unique,
+    phone char(11) unique,
     username varchar(128) unique,
     password varchar,
     image varchar unique,
