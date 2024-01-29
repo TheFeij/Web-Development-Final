c55ddf74d8cf77ca19582bf807cb6793b935d23b
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 18:39:47 +0330

    Added Init() function for connecting to the database

---
+++ db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	dsn := "host=localhost port=5432 user=root password=1234 dbname=messanger sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	DB = db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
