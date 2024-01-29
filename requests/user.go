0a422018bb463bbf14843a804628309a42d62692
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 19:58:51 +0330

    changed registerUser req struct to bind to forms instead of json to receive image alongside other information

---
+++ requests/user.go
@@ -0,0 +1,11 @@
+package requests
+
+type RegisterUser struct {
+	Username  string `form:"username" validate:"required;min=4;max=128"`
+	Password  string `form:"password" validate:"required;min=8;max=64"`
+	Firstname string `form:"firstname" validate:"required;min=1;max=50"`
+	Lastname  string `form:"lastname" validate:"required;min=1;max=50"`
+	Phone     string `form:"phone" validate:"required;size=11"`
+	Bio       string `form:"bio" validate:"max=100"`
+	Image     string
+}
