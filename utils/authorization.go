e8e4a54db24a4bd6863de904a20905d96193f83d
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 22:04:13 +0330

    merged two access token and refresh token functions

--- utils/authorization.go
+++ utils/authorization.go
@@ -10,30 +10,16 @@ type UserClaims struct {
 	jwt.RegisteredClaims
 }
 
-func NewAccessToken(claims UserClaims) (string, error) {
+func NewToken(claims UserClaims) (string, error) {
 	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return accessToken.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
 }
 
-func NewRefreshToken(claims jwt.RegisteredClaims) (string, error) {
-	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	return refreshToken.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
-}
-
-func ParseAccessToken(accessToken string) *jwt.Token {
+func ParseToken(accessToken string) *jwt.Token {
 	parsedAccessToken, _ := jwt.ParseWithClaims(accessToken, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("TOKEN_SECRET")), nil
 	})
 
 	return parsedAccessToken
-}
-
-func ParseRefreshToken(refreshToken string) *jwt.RegisteredClaims {
-	parsedRefreshToken, _ := jwt.ParseWithClaims(refreshToken, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(os.Getenv("TOKEN_SECRET")), nil
-	})
-
-	return parsedRefreshToken.Claims.(*jwt.RegisteredClaims)
 }
