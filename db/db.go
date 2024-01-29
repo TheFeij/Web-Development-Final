f06df0e1131b59c96aa4e7e8cc4cc70f5579b3f9
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 18:48:10 +0330

    fixed a typo in db name

--- db/db.go
+++ db/db.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	dsn := "host=localhost port=5432 user=root password=1234 dbname=messanger sslmode=disable"
+	dsn := "host=localhost port=5432 user=root password=1234 dbname=messenger sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
