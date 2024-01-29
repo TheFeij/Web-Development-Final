59c21c6dbadd7076cd0951023982b5adc47ee644
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-29 18:55:12 +0330

    defined handler struct, NewHandler() to receive a new handler and errResponse() to get a error response

---
+++ api/user/handler.go
@@ -0,0 +1,20 @@
+package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	db *gorm.DB
+}
+
+func NewHandler(db *gorm.DB) *Handler {
+	return &Handler{
+		db: db,
+	}
+}
+
+func errResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
