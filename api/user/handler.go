f722d05cb0a6ff3b5135e31544b8fad0f1a418c4
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-30 13:57:23 +0330

    defined login handler and refresh token handler

--- api/user/handler.go
+++ api/user/handler.go
@@ -109,6 +109,87 @@ func (h Handler) GetUserInformation(context *gin.Context) {
 	context.JSON(http.StatusOK, user)
 }
 
+func (h Handler) GetAccessToken(context *gin.Context) {
+	refreshToken := context.GetHeader("Refresh-Token")
+
+	if refreshToken == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing refresh token"})
+		return
+	}
+
+	parsedRefreshToken := utils.ParseToken(refreshToken)
+	if parsedRefreshToken == nil || parsedRefreshToken.Valid == false {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token not valid"})
+		return
+	}
+
+	// Attach the user claims to the context for use in the handlers
+	claims, ok := parsedRefreshToken.Claims.(*utils.UserClaims)
+	if !ok {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized - Invalid token claims"})
+		return
+	}
+
+	accessToken, err := utils.NewToken(utils.UserClaims{
+		ID: claims.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+		},
+	})
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	context.Header("Authorization", accessToken)
+	context.Header("Refresh-Token", refreshToken)
+	context.Status(http.StatusOK)
+}
+
+func (h Handler) Login(context *gin.Context) {
+	var req requests.LoginRequest
+	if err := context.ShouldBindJSON(&req); err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	service := services.New(h.db)
+	res, err := service.CheckLogin(req)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	refreshToken, err := utils.NewToken(utils.UserClaims{
+		ID: res.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+		},
+	})
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	accessToken, err := utils.NewToken(utils.UserClaims{
+		ID: res.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+		},
+	})
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	context.Header("Authorization", accessToken)
+	context.Header("Refresh-Token", refreshToken)
+	context.JSON(http.StatusOK, res)
+}
+
 func errResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
