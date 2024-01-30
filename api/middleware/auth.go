bb010c6dce39c5d3cc328fe83c2d2cbf76ab4165
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-30 14:17:28 +0330

    chaned auth middleware to use utils.ValidateToken function for access token validation

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -9,23 +9,9 @@ import (
 func AuthMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		accessToken := context.GetHeader("Authorization")
-
-		if accessToken == "" {
-			context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized - Missing access token"})
-			return
-		}
-
-		parsedToken := utils.ParseToken(accessToken)
-
-		if parsedToken == nil || !parsedToken.Valid {
-			context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized - Invalid access token"})
-			return
-		}
-
-		// Attach the user claims to the context for use in the handlers
-		claims, ok := parsedToken.Claims.(*utils.UserClaims)
-		if !ok {
-			context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized - Invalid token claims"})
+		claims, err := utils.ValidateToken(accessToken)
+		if err != nil {
+			context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			return
 		}
 
