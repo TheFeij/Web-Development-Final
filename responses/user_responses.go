d5b6a7225c507dcb5745299074c694679abe0345
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-30 20:35:39 +0330

    Added DisplayPhone and DisplayProfilePicture privacy fields

--- responses/user_responses.go
+++ responses/user_responses.go
@@ -1,12 +1,14 @@
 package responses
 
 type UserInformation struct {
-	ID        uint   `json:"id"`
-	Username  string `json:"username"`
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-	Phone     string `json:"phone"`
-	Bio       string `json:"bio"`
+	ID                    uint   `json:"id"`
+	Username              string `json:"username"`
+	Firstname             string `json:"firstname"`
+	Lastname              string `json:"lastname"`
+	Phone                 string `json:"phone"`
+	Bio                   string `json:"bio"`
+	DisplayPhone          bool   `json:"display_phone"`
+	DisplayProfilePicture bool   `json:"display_profile_picture"`
 }
 
 type UsersSearch struct {
