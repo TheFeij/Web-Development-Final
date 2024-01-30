f398987b5dcdbe010c5ed9fe7cb21ccc19f79135
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-30 20:36:38 +0330

    Added handlers to update, delete and setProfile

--- api/user/handler.go
+++ api/user/handler.go
@@ -3,14 +3,13 @@ package user
 import (
 	"Messenger/db/services"
 	"Messenger/requests"
-	"Messenger/responses"
 	"Messenger/utils"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
 	"path/filepath"
 	"strconv"
-	"strings"
 )
 
 type Handler struct {
@@ -31,35 +30,12 @@ func (h Handler) RegisterUser(context *gin.Context) {
 		return
 	}
 
-	// Handle image upload
-	file, err := context.FormFile("image")
-	hasImage := false
-	if err == nil {
-		hasImage = true
-		ext := filepath.Ext(file.Filename)
-		if ext != ".jpg" && ext != ".png" {
-			context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid file extension. Supported formats: jpg, png"})
-			return
-		}
-
-		imagePath := "./data/profile_images/" + req.Username + ext
-		req.Image = imagePath
-	}
-
 	//save the user
 	service := services.New(h.db)
-	var res responses.UserInformation
-	res, err = service.RegisterUser(req)
+	res, err := service.RegisterUser(req)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, errResponse(err))
 		return
-	}
-
-	if hasImage {
-		if err := context.SaveUploadedFile(file, req.Image); err != nil {
-			context.JSON(http.StatusInternalServerError, errResponse(err))
-			return
-		}
 	}
 
 	refreshToken, err := utils.NewRefreshToken(res.ID)
@@ -79,16 +55,46 @@ func (h Handler) RegisterUser(context *gin.Context) {
 	context.JSON(http.StatusOK, res)
 }
 
-func (h Handler) GetUserInformation(context *gin.Context) {
-	service := services.New(h.db)
+func (h Handler) SetProfilePicture(context *gin.Context) {
 
-	userID, err := strconv.ParseUint(strings.TrimSpace(context.Param("user_id")), 10, 64)
+	claims, err := getClaims(context)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+	}
+
+	file, err := context.FormFile("image")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+	}
+	ext := filepath.Ext(file.Filename)
+	if ext != ".jpg" && ext != ".png" {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid file extension. Supported formats: jpg, png"})
 		return
 	}
 
-	user, err := service.GetUserInfo(uint(userID))
+	imagePath := "./data/profile_images/" + strconv.FormatUint(uint64(claims.ID), 10) + ext
+
+	service := services.New(h.db)
+	if err := service.SetProfileImage(claims.ID, imagePath); err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	if err := context.SaveUploadedFile(file, imagePath); err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+}
+
+func (h Handler) GetUserInformation(context *gin.Context) {
+
+	claims, err := getClaims(context)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+	}
+
+	service := services.New(h.db)
+	user, err := service.GetUserInfo(claims.ID)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, errResponse(err))
 	}
@@ -147,6 +153,57 @@ func (h Handler) Login(context *gin.Context) {
 	context.JSON(http.StatusOK, res)
 }
 
+func (h Handler) DeleteUser(context *gin.Context) {
+	claims, err := getClaims(context)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+	}
+
+	service := services.New(h.db)
+	res, err := service.DeleteUser(claims.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	context.JSON(http.StatusOK, res)
+}
+
+func (h Handler) UpdateUser(context *gin.Context) {
+	var req requests.RegisterUser
+	if err := context.ShouldBind(&req); err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	claims, err := getClaims(context)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+	}
+
+	service := services.New(h.db)
+	res, err := service.UpdateUser(req, claims.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	context.JSON(http.StatusOK, res)
+}
+
 func errResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func getClaims(context *gin.Context) (utils.UserClaims, error) {
+	var claims utils.UserClaims
+	value, _ := context.Get("userClaims")
+	if userClaims, ok := value.(utils.UserClaims); ok {
+		claims = userClaims
+	} else {
+		context.Status(http.StatusInternalServerError)
+		return utils.UserClaims{}, errors.New("could not get the claims")
+	}
+
+	return claims, nil
 }
