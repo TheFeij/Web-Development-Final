a05835f3471a39b39b86df9c7f4612733cfc2f43
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-30 19:40:27 +0330

    Added two provacy fileds to RegisterUser request struct

--- requests/user_requests.go
+++ requests/user_requests.go
@@ -1,13 +1,15 @@
 package requests
 
 type RegisterUser struct {
-	Username  string `form:"username" validate:"required;min=4;max=128"`
-	Password  string `form:"password" validate:"required;min=8;max=64"`
-	Firstname string `form:"firstname" validate:"required;min=1;max=50"`
-	Lastname  string `form:"lastname" validate:"required;min=1;max=50"`
-	Phone     string `form:"phone" validate:"required;size=11"`
-	Bio       string `form:"bio" validate:"max=100"`
-	Image     string
+	Username              string `form:"username" validate:"required;min=4;max=128"`
+	Password              string `form:"password" validate:"required;min=8;max=64"`
+	Firstname             string `form:"firstname" validate:"required;min=1;max=50"`
+	Lastname              string `form:"lastname" validate:"required;min=1;max=50"`
+	Phone                 string `form:"phone" validate:"required;size=11"`
+	Bio                   string `form:"bio" validate:"max=100"`
+	Image                 string
+	DisplayPhone          bool `form:"display_phone"`
+	DisplayProfilePicture bool `form:"display_profile_picture"`
 }
 
 type LoginRequest struct {
