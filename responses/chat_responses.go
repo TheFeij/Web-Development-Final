b86ac845830fc13d2029decf31f80aefdba4b68d
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-31 19:04:29 +0330

    Defined ChatList response struct

--- responses/chat_responses.go
+++ responses/chat_responses.go
@@ -1,9 +1,15 @@
 package responses
 
-import "time"
+import (
+	"time"
+)
 
 type Chat struct {
 	ID        uint      `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	IsDead    bool      `json:"is_dead"`
+}
+
+type ChatsList struct {
+	Chats []Chat `json:"chats"`
 }
