4503d95f6236a999ecf3ccda985fbfe531542270
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-01-31 19:12:46 +0330

    Defined ChatContent and ChatMessage response struct

--- responses/chat_responses.go
+++ responses/chat_responses.go
@@ -13,3 +13,19 @@ type Chat struct {
 type ChatsList struct {
 	Chats []Chat `json:"chats"`
 }
+
+type ChatContent struct {
+	Chat     Chat          `json:"chat"`
+	Messages []ChatMessage `json:"messages"`
+}
+
+type ChatMessage struct {
+	ID                uint      `json:"id"`
+	ChatID            uint      `json:"chat_id"`
+	SourceSenderID    uint      `json:"source_sender_id"`
+	OriginalMessageID uint      `json:"original_message_id"`
+	SenderID          uint      `json:"sender_id"`
+	ReceiverID        uint      `json:"receiver_id"`
+	Content           string    `json:"content"`
+	CreatedAt         time.Time `json:"created_at"`
+}
