f10510ab4e64b5f6ea6fb60c1c8bc330e4969618
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-01 21:17:52 +0330

    Added all the route handlers to their related endpoint

--- api/server.go
+++ api/server.go
@@ -26,18 +26,49 @@ func NewServer(db *gorm.DB) *Server {
 		})
 	})
 
-	service := services.New(db)
-	userHandler := handlers.NewHandler(server.db, service.UserServices)
-	server.router.POST("/api/register", userHandler.RegisterUser)
-	server.router.GET("/api/refresh-token", userHandler.GetAccessToken)
-	server.router.POST("/api/login", userHandler.Login)
-	protectedRoute := server.router.Group("/api/handlers")
+	handler := handlers.NewHandler(server.db, services.New(db))
+	server.router.POST("/api/register", handler.UserHandler.RegisterUser)
+	server.router.GET("/api/refresh-token", handler.UserHandler.GetAccessToken)
+	server.router.POST("/api/login", handler.UserHandler.Login)
+	usersRouter := server.router.Group("/api/users")
 	{
-		protectedRoute.Use(middleware.AuthMiddleware())
-		protectedRoute.GET("/:user_id", userHandler.GetUserInformation)
-		protectedRoute.POST("/set_profile_image", userHandler.SetProfilePicture)
-		protectedRoute.PATCH("/", userHandler.UpdateUser)
-		protectedRoute.DELETE("/", userHandler.DeleteUser)
+		usersRouter.Use(middleware.AuthMiddleware())
+		usersRouter.GET("/:user_id", handler.UserHandler.GetUserInformation)
+		usersRouter.POST("/set_profile_image", handler.UserHandler.SetProfilePicture)
+		usersRouter.PATCH("/", handler.UserHandler.UpdateUser)
+		usersRouter.DELETE("/", handler.UserHandler.DeleteUser)
+
+		usersRouter.GET("/contacts", handler.ContactHandler.GetContacts)
+		usersRouter.POST("/contacts", handler.ContactHandler.AddContact)
+		usersRouter.DELETE("/contacts/:contact_id", handler.ContactHandler.DeleteContact)
+	}
+
+	chatsRouter := server.router.Group("/api/chats")
+	{
+		chatsRouter.Use(middleware.AuthMiddleware())
+		chatsRouter.POST("/", handler.ChatHandler.CreateChat)
+		chatsRouter.GET("/", handler.ChatHandler.GetChats)
+		chatsRouter.GET("/:chat_id", handler.ChatHandler.GetChatContent)
+		chatsRouter.DELETE("/:chat_id", handler.ChatHandler.DeleteChat)
+		chatsRouter.DELETE("/:chat_id/:message_id", handler.ChatHandler.DeleteChatMessage)
+	}
+
+	groupsRouter := server.router.Group("/api/groups")
+	{
+		groupsRouter.Use(middleware.AuthMiddleware())
+		groupsRouter.POST("/", handler.GroupHandler.CreateGroup)
+		groupsRouter.DELETE("/:group_id", handler.GroupHandler.DeleteGroup)
+		groupsRouter.PATCH("/:group_id", handler.GroupHandler.AddMember)
+		groupsRouter.DELETE("/:group_id/:user_id", handler.GroupHandler.DeleteMember)
+	}
+
+	channelsRouter := server.router.Group("/api/channels")
+	{
+		channelsRouter.Use(middleware.AuthMiddleware())
+		channelsRouter.POST("/", handler.ChannelHandler.CreateChannel)
+		channelsRouter.DELETE("/:channels_id", handler.ChannelHandler.DeleteChannel)
+		channelsRouter.PATCH("/:channels_id", handler.ChannelHandler.AddMember)
+		channelsRouter.DELETE("/:channels_id/:user_id", handler.ChannelHandler.DeleteMember)
 	}
 
 	return server
