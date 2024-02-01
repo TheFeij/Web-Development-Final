3536dc18a2fec4619e1789b11f7391acb7e45986
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-01 12:00:43 +0330

    Added getUserContacts method

--- db/services/contacts_services.go
+++ db/services/contacts_services.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"Messenger/db/models"
+	"Messenger/responses"
 	"gorm.io/gorm"
 )
 
@@ -12,4 +14,18 @@ func New(db *gorm.DB) *ContactsServices {
 	return &ContactsServices{
 		DB: db,
 	}
+}
+
+func (contactsServices *ContactsServices) getUserContacts(userID uint) (responses.ContactsList, error) {
+	var contactsList responses.ContactsList
+
+	// Fetch specific attributes (ContactID, ContactName) for the given userID from the database
+	if err := contactsServices.DB.Model(&models.Contact{}).
+		Select("contact_id, contact_name").
+		Where("user_id = ?", userID).
+		Find(&contactsList.Contacts).Error; err != nil {
+		return contactsList, err
+	}
+
+	return contactsList, nil
 }
