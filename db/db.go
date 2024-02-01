148e36bae2595c26b2fee2e80f338fda5e780169
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-01 22:17:51 +0330

    corrected dsn string, it was changed duo to an automatic rename

--- db/db.go
+++ db/db.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	dsn := "host=localhost port=5432 handlers=root password=1234 dbname=messenger sslmode=disable"
+	dsn := "host=localhost port=5432 user=root password=1234 dbname=messenger sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
