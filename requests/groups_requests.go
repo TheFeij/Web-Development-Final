ec5ba848f1aadc9c1b0ad62784152ee739288bbc
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-01 18:33:52 +0330

    Defined AddMember response struct

--- requests/groups_requests.go
+++ requests/groups_requests.go
@@ -1,6 +1,10 @@
 package requests
 
 type CreatGroup struct {
-	Name    string `json:"name" validator:"required;min=1;max=64"`
-	OwnerID uint   `json:"owner_id" validator:"required"`
+	Name    string `json:"name" validate:"required;min=1;max=64"`
+	OwnerID uint   `json:"owner_id" validate:"required"`
+}
+
+type AddMember struct {
+	UserID uint `json:"user_id" validate:"required"`
 }
