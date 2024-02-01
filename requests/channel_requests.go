c84207a2f67a48bb90b69f391b0ad9aa8701b5d7
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-01 19:41:28 +0330

    fixed a typo in struct name

--- requests/channel_requests.go
+++ requests/channel_requests.go
@@ -1,6 +1,6 @@
 package requests
 
-type CreatChannel struct {
+type CreateChannel struct {
 	Name    string `json:"name" validate:"required;min=1;max=64"`
 	OwnerID uint   `json:"owner_id" validate:"required"`
 }
