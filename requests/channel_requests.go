c8ca123f20a3e32e31d97ef6d36c95e6f29755eb
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-01 18:34:59 +0330

    fixe a mistake: replaced validator with validate

--- requests/channel_requests.go
+++ requests/channel_requests.go
@@ -1,6 +1,6 @@
 package requests
 
 type CreatChannel struct {
-	Name    string `json:"name" validator:"required;min=1;max=64"`
-	OwnerID uint   `json:"owner_id" validator:"required"`
+	Name    string `json:"name" validate:"required;min=1;max=64"`
+	OwnerID uint   `json:"owner_id" validate:"required"`
 }
