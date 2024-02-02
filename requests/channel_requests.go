95e37034e61afc5475b6ea30f7d16ed7ccfff378
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-02 21:40:10 +0330

    deleted the ownerID from CreateChannel. ownerID can be obtained from access token

--- requests/channel_requests.go
+++ requests/channel_requests.go
@@ -1,6 +1,5 @@
 package requests
 
 type CreateChannel struct {
-	Name    string `json:"name" validate:"required;min=1;max=64"`
-	OwnerID uint   `json:"owner_id" validate:"required"`
+	Name string `json:"name" validate:"required;min=1;max=64"`
 }
